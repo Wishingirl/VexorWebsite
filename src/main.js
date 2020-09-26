7ba341e378ae9c5a4f900778b8541a8eacb6aa87
Shigetorum <undecised@gmail.com>
2020-09-26 22:19:09 +0200

    add fish ui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
+//Imports
 import Vue from 'vue'
 import App from './App.vue'
-
+import FishUI from 'fish-ui'
+import VueFire from 'vuefire';
+//Config
 Vue.config.productionTip = false
+//Middlewares
+Vue.use(VueFire);
+Vue.use(FishUI)
 
+//Starting Vue
 new Vue({
   render: h => h(App),
 }).$mount('#app')
